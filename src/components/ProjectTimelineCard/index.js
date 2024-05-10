c5bbc06526c05be1d62d1924dfd453139ba8bf84
vineethsankre <lsvineethsankre@gmail.com>
2024-05-10 07:20:45 +0000

    index.css deleted

--- src/components/ProjectTimelineCard/index.js
+++ src/components/ProjectTimelineCard/index.js
@@ -13,13 +13,8 @@ import {
 
 const ProjectTimelineCard = props => {
   const {projectDetails} = props
-  const {
-    imageUrl,
-    projectTitle,
-    description,
-    projectUrl,
-    duration,
-  } = projectDetails
+  const {imageUrl, projectTitle, description, projectUrl, duration} =
+    projectDetails
 
   return (
     <ProjectCardContainer>
